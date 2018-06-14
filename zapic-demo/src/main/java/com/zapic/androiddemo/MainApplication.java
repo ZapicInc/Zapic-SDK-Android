09d6e91d2157e9177a2e2b893b949ea4f86bdca6
Kyle Dodson <kyledodson@gmail.com>
2018-06-14 13:30:25 -0700

    Restored Branch and OneSignal functionality in demo app

--- zapic-demo/src/main/java/com/zapic/androiddemo/MainApplication.java
+++ zapic-demo/src/main/java/com/zapic/androiddemo/MainApplication.java
@@ -1,70 +1,61 @@
 package com.zapic.androiddemo;
 
-import android.app.Application;
+import android.support.annotation.NonNull;
 
-//import com.onesignal.OSNotificationOpenResult;
-//import com.onesignal.OneSignal;
-
+import com.onesignal.OSNotificationOpenResult;
+import com.onesignal.OneSignal;
 import com.zapic.sdk.android.Zapic;
-//import com.zapic.sdk.android.ZapicPlayer;
+import com.zapic.sdk.android.ZapicPlayer;
+import com.zapic.sdk.android.ZapicPlayerAuthenticationHandler;
 
-//import org.json.JSONObject;
+import org.json.JSONObject;
 
-//import io.branch.referral.Branch;
-//import io.branch.referral.BranchApp;
+import io.branch.referral.Branch;
+import io.branch.referral.BranchApp;
 
-public class MainApplication extends Application { // BranchApp {
+public class MainApplication extends BranchApp {
     @Override
     public void onCreate() {
         super.onCreate();
 
-//        // Initialize Branch.
-//        final Branch branch = Branch.getAutoInstance(this);
+        // Initialize Branch.
+        final Branch branch = Branch.getAutoInstance(this);
 
-//        // Initialize OneSignal.
-//        OneSignal.startInit(this)
-//                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
-//                .unsubscribeWhenNotificationsAreDisabled(true)
-//                .setNotificationOpenedHandler(new OneSignal.NotificationOpenedHandler() {
-//                    @Override
-//                    public void notificationOpened(OSNotificationOpenResult result) {
-//                        JSONObject data = result.notification.payload.additionalData;
-//                        if (data != null) {
-//                            Zapic.handleInteractionEvent(data);
-//                        }
-//                    }
-//                })
-//                .init();
+        // Initialize OneSignal.
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .setNotificationOpenedHandler(new OneSignal.NotificationOpenedHandler() {
+                    @Override
+                    public void notificationOpened(OSNotificationOpenResult result) {
+                        JSONObject data = result.notification.payload.additionalData;
+                        if (data != null) {
+                            Zapic.handleInteraction(data);
+                        }
+                    }
+                })
+                .init();
 
         // Initialize Zapic.
         Zapic.start(this);
-//        final ZapicPlayer currentPlayer = Zapic.getCurrentPlayer();
-//        if (currentPlayer == null) {
-//            branch.logout();
-//            OneSignal.deleteTag("zapic_player_token");
-//        } else {
-//            branch.setIdentity(currentPlayer.getPlayerId());
-//            OneSignal.sendTag("zapic_player_token", currentPlayer.getPlayerId());
-//        }
+        Zapic.setPlayerAuthenticationHandler(new ZapicPlayerAuthenticationHandler() {
+            @Override
+            public void onLogin(@NonNull ZapicPlayer player) {
+                // Associate player with Branch session.
+                branch.setIdentity(player.getPlayerId());
 
-//        Zapic.start(new Zapic.AuthenticationHandler() {
-//            @Override
-//            public void onLogin(@NonNull ZapicPlayer player) {
-//                // Associate player with Branch session.
-//                branch.setIdentity(player.getPlayerId());
-//
-//                // Associate player/device with OneSignal for push notifications.
-//                OneSignal.sendTag(Zapic.NOTIFICATION_TAG, player.getNotificationToken());
-//            }
-//
-//            @Override
-//            public void onLogout(@NonNull ZapicPlayer player) {
-//                // Disassociate player from Branch session.
-//                branch.logout();
-//
-//                // Disassociate player/device from OneSignal for push notifications.
-//                OneSignal.deleteTag(Zapic.NOTIFICATION_TAG);
-//            }
-//        });
+                // Associate player/device with OneSignal for push notifications.
+                OneSignal.sendTag("zapic_player_token", player.getNotificationToken());
+            }
+
+            @Override
+            public void onLogout(@NonNull ZapicPlayer player) {
+                // Disassociate player from Branch session.
+                branch.logout();
+
+                // Disassociate player/device from OneSignal for push notifications.
+                OneSignal.deleteTag("zapic_player_token");
+            }
+        });
     }
 }
